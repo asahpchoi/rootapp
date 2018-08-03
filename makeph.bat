3b2eb1f653c848db21f232a6b22ff152b8a461e4
Asa HP Choi <asa_hp_choi@manulife.com>
2018-08-03 15:45:23 +0800

    X

--- makeph.bat
+++ makeph.bat
@@ -1,4 +1,10 @@
 copy .gitmodules.ph .gitmodules
 git pull
 cd src/app
+del local /s/q
+rmdir local
+git add .
+git commit -m "x"
+git submodule add --force https://github.com/asahpchoi/ph.git local
 git submodule update local
+cd ../..
